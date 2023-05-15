b420144a8a08133e97d063c3f45431b065e7364a
MartinFauth <101270871+MartinFauth@users.noreply.github.com>
2023-05-15 22:38:46 +0200

    new file:   PesuedoCode.txt 	modified:   README.md 	new file:   image1.png 	new file:   image2.png 	new file:   image3.jpg 	modified:   userMass.py

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Twitter-UserMass-alg
-twitter UserMass algorithm in python to test stuff
+
+traduction en Python du code userMass.scala de twitter avec un objet modifiable pour pouvoir calculer sa masse ("réputation") (https://github.com/twitter/the-algorithm.git)
+Ce code attribue un score de réputation à un utilisateur en fonction des paramètres par défault suivants :
+
+userId | "123"
+age | 50
+isRestricted | False
+isSuspended | False
+isVerified | False
+hasValidDevice | True
+numFollowers | 1000
+numFollowings | 1000
+deactivated | False
+
+Ce score est utilisé pour savoir quels utilisateurs méritents d'être mis en avant plus que d'autres. 
+
+En modifiant des paramètres on peut observer les comportements suivants:
+
+- modification du nombre de followers (abonnées)(voir image1.png)
+
+- modification du nombre de followings(abonnements) (voir image2.png)
+
+- modification de l'age d'un compte(en jours) (voir image3.png)
+
+- isSuspended donne une masse nulle
+
+- isVerified permet de commencer avec une masse de 100
+
+- isRestricted divise la masse par 10
+
+- hasValidDevice fait commencer à 5 si faux 
+
+- deactivated donne une masse nulle. 
+
+
+
+Pour run le programme :
+        python userMass.py
--- userMass.py
+++ userMass.py
@@ -4,18 +4,18 @@ import time
 
 userOne = {
 	"userId": "123",
-	"age": 1651033778,
+	"age": 50,
 	"isRestricted": False,
 	"isSuspended": False,
 	"isVerified": False,
 	"hasValidDevice": True,
-	"numFollowers": 700000,
-	"numFollowings": 700000,
+	"numFollowers": 1000,
+	"numFollowings": 1000,
 	"deactivated": False,
 }
 
 def getUserMass(userData):
-	currentTimestamp = time.time()
+
 	constantDivisionFactorGt_threshFriendsToFollowersRatioUMass = 5
 	threshAbsNumFriendsUMass = 500
 	threshFriendsToFollowersRatioMass = 0.6
@@ -23,7 +23,7 @@ def getUserMass(userData):
 	ageWeightAdditive = 0.2
 	restrictedWeightMultiplicative = 0.1
 
-	age = currentTimestamp - userData["age"]
+	age = userData["age"]
 
 	if userData["userId"] == "" or userData["deactivated"] == True:
 		return None
@@ -40,10 +40,10 @@ def getUserMass(userData):
 			else:
 				score += 0
 
-			if age > 30 * 86400:
+			if age > 25:
 				normalizedAge = 1
 			else:
-				normalizedAge = math.log(1 + (age / 15) * (1/86400))
+				normalizedAge = (math.log(1 + (age / 15)))
 
 			score *= normalizedAge
 
@@ -57,10 +57,8 @@ def getUserMass(userData):
 
 			mass = score
 
-			print("mass", mass)
 
 		friendToFollowersRatio = (1 + userData["numFollowings"]) / (1 + userData["numFollowers"])
-		print("friendToFollowersRatio", friendToFollowersRatio)
 
 		adjustedMass = mass
 
@@ -68,6 +66,7 @@ def getUserMass(userData):
 			adjustedMass = mass / math.exp(constantDivisionFactorGt_threshFriendsToFollowersRatioUMass * (friendToFollowersRatio - threshFriendsToFollowersRatioMass))
 
 		return adjustedMass
+
 
 returnedMass = getUserMass(userOne)
 print("User", userOne) 
